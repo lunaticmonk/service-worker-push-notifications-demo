603a40e56ec42ec47e90d37ebcf9c305c11add70
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-04-07 13:57:48 +0530

    update server.js

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.get('/', function(req, res) {
 });
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET, OPTIONS');
+  res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
+  res.header('Access-Control-Allow-Credentials', true);
   next();
  });
 
