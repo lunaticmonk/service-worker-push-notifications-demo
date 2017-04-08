f2cdf5c177db1e455807942bb20635e9ed12c7fb
Sumedh Nimkarde <blesssumedh@gmail.com>
2017-04-08 13:48:06 +0530

    update server.js

--- server.js
+++ server.js
@@ -20,8 +20,9 @@ app.set('view engine', 'ejs');
 const PORT = process.env.PORT || 5000;
 
 app.all('/', function(req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Origin", "https://pushservicee.herokuapp.com");
   res.header("Access-Control-Allow-Headers", "X-Requested-With");
+  res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
   next();
  });
 
